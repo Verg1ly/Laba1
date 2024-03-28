68e94fdc6db01a285d47292ee7b92b801de46bee
Vergil <dedenchuka@bk.tu>
2024-03-28 21:59:53 +0700

    Добавление комментариев

--- rectangle.cpp
+++ rectangle.cpp
@@ -2,11 +2,23 @@
 #include<math.h>
 using namespace std;
 int main(){
+cout << "Введите длину двух сторон" << endl;
 int lngth, wdth;
 cin >>  lngth ;
 cin >>  wdth;
-int P = 2*(lngth+wdth); 
-int S = lngth*wdth;
-int diagonal = sqrt(lngth^2 + wdth^2);
-cout << P << endl << S << endl << diagonal << endl;
+int P = 2*(lngth+wdth); // расчёт периметра
+int S = lngth*wdth; // расчёт площади
+int diagonal = sqrt(lngth^2 + wdth^2); // расчёт диагонали
+cout << "Периметр равен: "<< P << endl << "Площадь равна: "<< S << endl << "Диагональ равна: "<< diagonal << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
